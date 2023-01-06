f26fec9a8739564fcf15d18cb677c0829512887b
Paskab012 <pascalkabikampan@gmail.com>
2023-01-06 12:11:10 +0200

    Implement section organisees

--- src/components/formations/index.jsx
+++ src/components/formations/index.jsx
@@ -9,18 +9,17 @@ const FormationsOrganisees = () => {
 <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-wrap -mx-4 -mb-10 text-center">
-      <div className="sm:w-1/2 mb-10 px-4">
+      <div className="px-4 mb-10 sm:w-1/2">
         <div className="rounded-lg h-[80%] overflow-hidden">
-          <img alt="content" className="object-cover object-center h-full w-full" src={Acc} />
+          <img alt="content" className="object-cover object-center w-full h-full" src={Acc} />
         </div>
-        <h2 className="title-font text-3xl font-bold text-black mt-6 mb-3">Comptabilité</h2>
-       
+        <h2 className="mt-6 mb-3 text-3xl font-bold text-black title-font">Comptabilité</h2>
       </div>
-      <div className="sm:w-1/2 mb-10 px-4">
+      <div className="px-4 mb-10 sm:w-1/2">
         <div className="rounded-lg h-[80%] overflow-hidden">
-          <img alt="content" className="object-cover object-center h-full w-full" src={Fin} />
+          <img alt="content" className="object-cover object-center w-full h-full" src={Fin} />
         </div>
-        <h2 className="title-font text-3xl font-bold text-black mt-6 mb-3">Informatique de gestion</h2>
+        <h2 className="mt-6 mb-3 text-3xl font-bold text-black title-font">Informatique de gestion</h2>
        
       </div>
     </div>
