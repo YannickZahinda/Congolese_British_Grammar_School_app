f32359fc2ea112ad79bb47097086342b3916f903
omarbabou <obabou50@gmail.com>
2023-01-10 11:09:37 +0200

    Update the school page

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,56 +1,96 @@
-import React from 'react'
+import React from "react";
+import LoginImage from "../../assets/login.png";
 
 const Login = () => {
   return (
-<div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-  <div className="w-full max-w-md space-y-8">
-    <div>
-      <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/>
-      <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-      <p className="mt-2 text-center text-sm text-gray-600">
-        Or
-        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
-      </p>
+    <div className='relative flex w-full h-full text-black '>
+      <div className='w-1/2 h-screen '>
+        <div className='flex flex-col justify-center w-2/3 h-full mx-auto text-white xl:w-1/2'>
+          <div>
+            <p className='mb-3 text-3xl font-bold text-black'>Welcome back</p>
+          </div>
+
+          <div className='mt-10'>
+            <form>
+              <div>
+                <label
+                  className='mb-2.5 block font-normal text-black'
+                  for='email'
+                >
+                  Username or email
+                </label>
+                <input
+                  type='email'
+                  id='email'
+                  className='inline-block w-full p-4 leading-none placeholder-[#B1B1B1] bg-white border border-1 border-gray-200 text-black rounded-lg text-3lack '
+                  placeholder='Enter username or email'
+                />
+              </div>
+              <div className='mt-4'>
+                <label
+                  className='mb-2.5 block font-normal text-black'
+                  for='email'
+                >
+                  Password
+                </label>
+                <input
+                  type='password'
+                  id='email'
+                  className='inline-block w-full p-4 leading-none text-black placeholder-[#B1B1B1] bg-white border border-1 border-gray-200 rounded-lg'
+                  placeholder='Enter password'
+                />
+              </div>
+              <div className='flex flex-col justify-end w-full mt-4 sm:flex-row'>
+                <div>
+                  <a
+                    href='#'
+                    className='text-sm hover:text-gray-600 text-[#184C72]'
+                  >
+                    <span className='text-black'>Forgot password? </span>
+                    Reset password
+                  </a>
+                </div>
+              </div>
+              <div className='my-10'>
+                <button className='w-full p-4 bg-[#184C72] rounded-lg hover:bg-[#184C71]'>
+                  Login
+                </button>
+              </div>
+              <div className='mt-4'>
+                <fieldset className='border-t border-gray-400 border-solid'>
+                  <legend className='px-2 mx-auto text-sm text-center text-black'>
+                    OR
+                  </legend>
+                </fieldset>
+              </div>
+              <div className='flex justify-center mt-4'>
+                <p className='text-black'>
+                  Don't have an account yet?{" "}
+                  <span className='text-[#184C72] font-semibold cursor-pointer hover:font-bold'>
+                    {" "}
+                    Create account
+                  </span>{" "}
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class='h-screen w-1/2 relative '>
+        <div className='flex items-center content-center justify-center bg-red-300'>
+          <p className='absolute text-white  w-[310px] text-3xl text-center'>
+            L' école Congolese B.G.S. offre un cadre éducatif qui rassure la
+            communauté de la ville de Kolwezi...
+          </p>
+        </div>
+
+        <img
+          src={LoginImage}
+          className='hidden object-cover w-full h-full lg:block'
+        />
+      </div>
     </div>
-    <form className="mt-8 space-y-6" action="#" method="POST">
-      <input type="hidden" name="remember" value="true"/>
-      <div className="-space-y-px rounded-md shadow-sm">
-        <div>
-          <label for="email-address" className="sr-only">Email address</label>
-          <input id="email-address" name="email" type="email" autocomplete="email" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address"/>
-        </div>
-        <div>
-          <label for="password" className="sr-only">Password</label>
-          <input id="password" name="password" type="password" autocomplete="current-password" required className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password"/>
-        </div>
-      </div>
+  );
+};
 
-      <div className="flex items-center justify-between">
-        <div className="flex items-center">
-          <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"/>
-          <label for="remember-me" className="ml-2 block text-sm text-gray-900">Remember me</label>
-        </div>
-
-        <div className="text-sm">
-          <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
-        </div>
-      </div>
-
-      <div>
-        <button type="submit" className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-            <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
-            </svg>
-          </span>
-          Sign in
-        </button>
-      </div>
-    </form>
-  </div>
-</div>
-
-  )
-}
-
-export default Login
+export default Login;
